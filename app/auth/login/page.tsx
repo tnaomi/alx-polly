0b21db2e157f612a188523a19e6c0d4eef1aeec3
tnaomi <naomikapengule@gmail.com>
2025-09-21 20:15:51 +0200

    Integrate  form action

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
+import { loginUser } from "@/lib/authActions";
 import Link from "next/link";
+import { useFormState } from "react-dom";
 
 export default function Login() {
+  const [formState, formAction] = useFormState(loginUser, { errors: undefined });
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <h1 className="text-2xl font-bold mb-4">Login</h1>
-      <form className="w-full max-w-sm space-y-4">
+      {formState.errors && formState.errors.map((error) => <p className="text-red-500">{error}</p>)}
+      <form action={formAction} className="w-full max-w-sm space-y-4">
         <div>
           <label htmlFor="email" className="block text-sm font-medium mb-1">Email</label>
           <Input id="email" name="email" type="email" autoComplete="email" required className="w-full" />
