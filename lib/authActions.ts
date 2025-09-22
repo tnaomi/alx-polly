8d547e68b67cb3505794e8411c7d1cfa84b20697
tnaomi <naomikapengule@gmail.com>
2025-09-22 04:54:42 +0200

    Added success return state.
    persisted signup data to User

--- lib/authActions.ts
+++ lib/authActions.ts
@@ -3,9 +3,11 @@
 import { redirect } from "next/navigation";
 import { createClient } from "./supabaseServer";
 import { Session, User } from "@supabase/supabase-js";
+import bcrypt from "bcryptjs";
 
 type State = {
   errors?: string[];
+  success?: string | undefined;
   user?: User;
   session?: Session;
 };
@@ -49,15 +51,27 @@ export async function logoutUser() {
 }
 
 export async function signupUser(
-  prevState: { error: string | undefined },
+  prevState: State,
   formData: FormData
 ) {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
+
   const supabase = await createClient();
-  const { error } = await supabase.auth.signUp({ email, password });
-  if (!error) {
-    redirect("/auth/protected-page");
+  const { data, error } = await supabase.auth.signUp({ email, password });
+
+  if (!error && data.user) {
+    // Insert user into User table
+    const { error: insertError } = await supabase
+      .from('User')
+      .insert([
+        { email: data.user.email, password: await bcrypt.hash(password, 10) },
+      ]);
+
+    if (insertError) {
+      return { errors: [insertError.message] };
+    }
+    return {success: 'Your account has been created. Please check your email to verify your account.'}
   }
-  return { error: error?.message };
+  return { errors: [error?.message] };
 }
